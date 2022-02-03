a51f26f520e0fe080d5b8715f05e3705e6ca5499
Marcelo Viana <marcelorsviana@gmail.com>
2022-02-03 20:23:36 -0300

    Task 2 - Learn about pairs and triples

---
+++ src/main/kotlin/test/in/repl/PairsAndTriples.kt
@@ -0,0 +1,33 @@
+package test.`in`.repl
+
+/*
+Test the code below in Kotlin REPL and explore "pairs" and "triples"
+ */
+
+
+/*
+val equipment = "fish net" to "catching fish"
+println("${equipment.first} used for ${equipment.second}")
+fish net used for catching fish
+
+val numbers = Triple(6, 9, 42)
+println(numbers.toString())
+println(numbers.toList())
+(6, 9, 42)[6, 9, 42]
+
+val equipment2 = ("fish net" to "catching fish") to "equipment"
+println("${equipment2.first} is ${equipment2.second}\n")
+println("${equipment2.first.second}")
+(fish net, catching fish) is equipment
+catching fish
+
+val equipment = "fish net" to "catching fish"
+val (tool, use) = equipment
+println("$tool is used for $use")
+fish net is used for catching fish
+
+val numbers = Triple(6, 9, 12)
+val (n1, n2, n3) = numbers
+println("$n1 $n2 $n3")
+6 9 12
+*/

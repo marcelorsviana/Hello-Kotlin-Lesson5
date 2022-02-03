35db8354384523f5b7a6c98e9c25ddbc9a287fa7
Marcelo Viana <marcelorsviana@gmail.com>
2022-02-03 20:46:57 -0300

    Task 3 - Collections

---
+++ src/main/kotlin/test/in/repl/Collections.kt
@@ -0,0 +1,52 @@
+package test.`in`.repl
+
+/*
+Test the code below in Kotlin REPL and explore "collections"
+ */
+
+/*
+val list = listOf(1, 5, 3, 4)
+println(list.sum())
+13
+
+val list2 = listOf("a", "bb", "cccc")
+println(list2.sumBy{it.length})
+7
+
+val list2 = listOf("a", "bb", "cccc")
+for (s in list2.listIterator()) {
+    println("$s ")
+}
+a bb cccc
+
+val cures = hashMapOf("white spots" to "Ich", "red sores" to "hole disease")
+println(cures.get("white spots"))
+Ich
+
+val cures = hashMapOf("white spots" to "Ich", "red sores" to "hole disease")
+println(cures["white spots"])
+Ich
+
+println(cures["red sores"])
+hole disease
+
+println(cures["scale loss"])
+null
+
+println(cures.getOrDefault("bloating", "sorry, I don't know"))
+sorry, I don't know
+
+println(cures.getOrElse("bloating") {"sorry, I don't know"})
+sorry, I don't know
+
+println(cures.getOrElse("red sores") {"sorry, I don't know"})
+hole disease
+
+val inventory = mutableMapOf("fish net" to 1)
+inventory.put("tank scrubber", 3)
+println(inventory.toString())
+inventory.remove("fish net")
+println(inventory.toString())
+{fish net=1, tank scrubber=3}{tank scrubber=3}
+
+ */
